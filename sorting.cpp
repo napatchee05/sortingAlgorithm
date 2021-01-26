eaeb9837d876acbea69c60cb4486415a35040d4f
Napat Cheetanom <napatcheetanom@Napats-MacBook-Pro.local>
2021-01-27 00:02:55 +0700

    add : radix sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+
 
 void printArray(int arr[],int n) {
     for (int i = 0 ; i < n ; i++) {
@@ -14,7 +16,7 @@ void bubbleSort(int arr[],int n) {
             if (arr[j] > arr[j+1]) {
                 std::swap(arr[j],arr[j+1]) ;
                 printArray(arr,n) ;
-            }
+            } 
         }
     }
 }
@@ -48,11 +50,63 @@ void insertionSort(int arr[] , int n) {
     }
 }
 
+bool isSorted(int arr[],int n) {
+    for (int i = 0 ; i < n-1 ; i++) {
+        if (arr[i] > arr[i+1]) {
+            return false ;
+        }
+    }
+    return true ;
+}
+
+int power(int base,int power) {
+    int result = 1 ;
+    for (int i = 0 ; i < power ; i++) {
+        result *= base ;
+    }
+    return result ;
+}
+
+int getDigitFL(int n , int digit) {
+    return (n/power(10,digit))%10 ;
+}
+
+int lengthOfInt(int n) {
+    int length = 0 ;
+    while (n) {
+        length += 1 ;
+        n /= 10 ;
+    }
+    return length ;
+}
+
+void radixSort(int arr[],int n) {
+    std::queue<int> qs[10] ;
+    int maxLength = lengthOfInt(arr[0]) ;
+    for (int i = 1 ; i < n ; i++) {
+        maxLength = lengthOfInt(arr[i]) > maxLength ? lengthOfInt(arr[i]) : maxLength ;
+    }
+    for (int i = 0 ; i < maxLength ; i++) {
+        int e = 0 ;
+        for (int j = 0 ; j < n ; j++) {
+            qs[getDigitFL(arr[j],i)].push(arr[j]) ;
+        }
+        for (int k = 0 ; k < 10 ; k++) {
+            while(!qs[k].empty()) {
+                arr[e++] = qs[k].front() ;
+                qs[k].pop() ;
+            }
+        }
+        printArray(arr,n) ;
+    }
+}
+
 int main() {
     int arr[] = {5,1,4,28,10,1,0} ;
     int n = sizeof(arr)/sizeof(int) ;
     printArray(arr,n) ;
-    insertionSort(arr,n) ;
+    radixSort(arr,n) ;
+    std::cout << getDigitFL(28,1) ;
 }
 
 
